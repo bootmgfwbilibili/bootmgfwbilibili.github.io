822da24124c1a24189238bb3e1abd35f871de1ed
bootmgfwbilibili <142314450+bootmgfwbilibili@users.noreply.github.com>
2024-07-12 18:29:16 +0800

    Add files via upload

--- main/home/function.js
+++ main/home/function.js
@@ -1,29 +1,45 @@
 function readme()
 {
-    cookie=document.cookie.split(',');
-    cookie[0]="0"
+    cookie=document.cookie.split(';');
+    cookienum=cookie[0];
+    cookienumcut=cookienum.split(',');
+    cookienumcut[0]="0"
+    cookienum=cookienumcut;
+    cookie[0]=cookienum;
     document.cookie=cookie;
     document.getElementById("welcome-dialog").innerHTML="";
     document.getElementById("welcome-dropdown").innerHTML="";
 }
 function num1()
 {
-    cookie=document.cookie.split(',');
-    cookie[1]=1;
+    cookie=document.cookie.split(';');
+    cookienum=cookie[0];
+    cookienumcut=cookienum.split(',');
+    cookienumcut[1]=1;
+    cookienum=cookienumcut;
+    cookie[0]=cookienum;
     document.cookie=cookie;
     document.getElementById("num1").innerHTML="";
 }
 function num2()
 {
-    cookie=document.cookie.split(',');
-    cookie[2]=1;
+    cookie=document.cookie.split(';');
+    cookienum=cookie[0];
+    cookienumcut=cookienum.split(',');
+    cookienumcut[2]=1;
+    cookienum=cookienumcut;
+    cookie[0]=cookienum;
     document.cookie=cookie;
     document.getElementById("num2").innerHTML="";
 }
 function num3()
 {
-    cookie=document.cookie.split(',');
-    cookie[3]=1;
+    cookie=document.cookie.split(';');
+    cookienum=cookie[0];
+    cookienumcut=cookienum.split(',');
+    cookienumcut[3]=1;
+    cookienum=cookienumcut;
+    cookie[0]=cookienum;
     document.cookie=cookie;
     document.getElementById("num3").innerHTML="";
 }
