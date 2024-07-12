c88cf8b1a739aa44bdbfe47c04928798c8205019
bootmgfwbilibili <142314450+bootmgfwbilibili@users.noreply.github.com>
2024-07-12 18:02:56 +0800

    Add files via upload

--- main/home/cookie/IntializeCookie.js
+++ main/home/cookie/IntializeCookie.js
@@ -1,5 +1,8 @@
 cookie=document.cookie.split(',')
 length=cookie.length;
+window.alert(document.cookie);
+window.alert(cookie);
+window.alert(length);
 if(length<4)
 {
     document.cookie="1,0,0,0";
