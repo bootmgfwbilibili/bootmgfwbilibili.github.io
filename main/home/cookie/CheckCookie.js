93974d8f998799f519244a49043663fc0dea0a11
bootmgfwbilibili <142314450+bootmgfwbilibili@users.noreply.github.com>
2024-07-28 17:44:06 +0800

    Add files via upload

--- main/home/cookie/CheckCookie.js
+++ main/home/cookie/CheckCookie.js
@@ -4,22 +4,22 @@ if(cookielength==0)
     document.cookie="num=1,0,0,0;expires=31 Dec 9999 23:59:59 GMT";
 }
 cookienum=document.cookie.split(';')[0].split('=')[1].split(',');
-var num1="1";var num2="1";var num3="1";
+varnum1="1";varnum2="1";varnum3="1";
 if(cookienum[0]!="1")
 {
     document.getElementById("welcome-dialog").innerHTML="";
     document.getElementById("dialog-header").innerHTML="欢迎回来";
     document.getElementById("welcome-dropdown").innerHTML="";
 }
-if(cookienum[1]==num1)
+if(cookienum[1]==varnum1)
 {
     document.getElementById("num1").innerHTML="";
 }
-if(cookienum[2]==num2)
+if(cookienum[2]==varnum2)
 {
     document.getElementById("num2").innerHTML="";
 }
-if(cookienum[3]==num3)
+if(cookienum[3]==varnum3)
 {
     document.getElementById("num3").innerHTML="";
 }
