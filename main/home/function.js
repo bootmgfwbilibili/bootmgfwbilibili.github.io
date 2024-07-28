87de2455e386a58b7df3e2bf7901b6b52b393276
bootmgfwbilibili <142314450+bootmgfwbilibili@users.noreply.github.com>
2024-07-28 16:39:38 +0800

    Add files via upload

--- main/home/function.js
+++ main/home/function.js
@@ -10,23 +10,20 @@ function num1()
 {
     cookienum=document.cookie.split(';')[0].split('=')[1].split(',');
     cookienum[1]="1";
-    cookie=cookienum+";expires=31 Dec 9999 23:59:59 GMT";
-    document.cookie=cookie;
+    document.cookie="num="+cookienum+";expires=31 Dec 9999 23:59:59 GMT";
     document.getElementById("num1").innerHTML="";
 }
 function num2()
 {
     cookienum=document.cookie.split(';')[0].split('=')[1].split(',');
     cookienum[2]="1";
-    cookie=cookienum+";expires=31 Dec 9999 23:59:59 GMT";
-    document.cookie=cookie;
+    document.cookie="num="+cookienum+";expires=31 Dec 9999 23:59:59 GMT";
     document.getElementById("num2").innerHTML="";
 }
 function num3()
 {
     cookienum=document.cookie.split(';')[0].split('=')[1].split(',');
     cookienum[3]="1";
-    cookie=cookienum+";expires=31 Dec 9999 23:59:59 GMT";
-    document.cookie=cookie;
+    document.cookie="num="+cookienum+";expires=31 Dec 9999 23:59:59 GMT";
     document.getElementById("num3").innerHTML="";
 }
